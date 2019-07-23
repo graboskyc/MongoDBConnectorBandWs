2c5ca49874a1d92bd89e6ca3ead3000c3e08ba40
Chris Grabosky <chris@grabosky.net>
2019-07-23 13:40:00 -0400

    Update tester.py

--- tester.py
+++ tester.py
@@ -6,22 +6,22 @@ def p(r):
         print(doc)
 
 print("===CONNECT===")
-gmc = gskymongofactory("mongodb+srv://atlasroot:password@beta-v9iwf.mongodb.net/test?retryWrites=true&w=majority&maxPoolSize=10")
+gmf = gskymongofactory("mongodb+srv://atlasroot:password@beta-v9iwf.mongodb.net/test?retryWrites=true&w=majority")
 
 print("===AGG===")
-r = gmc.doAgg('sample_mflix','movies',[{"$sample":{"size":1}}], appAutoRetry=30)
+r = gmf.doAgg('sample_mflix','movies',[{"$sample":{"size":1}}], appAutoRetry=30)
 p(r)
 
 print("===FINDONE===")
-r = gmc.doFindOne('sample_mflix','movies', {})
+r = gmf.doFindOne('sample_mflix','movies', {})
 print(r)
 print("===FINDONE===")
-r = gmc.doFindOne('sample_mflix','movies', {}, projection={"_id":0})
+r = gmf.doFindOne('sample_mflix','movies', {}, projection={"_id":0})
 print(r)
 #print("===FINDONE===")
-#r = gmc.doFindOne('baddbname','movies', {})
+#r = gmf.doFindOne('baddbname','movies', {})
 #print(r)
 
 print("===INSONE===")
-r = gmc.doInsertOne('sample_mflix','movies', {"title":"My great movie"})
+r = gmf.doInsertOne('sample_mflix','movies', {"title":"My great movie"})
 print(r)
